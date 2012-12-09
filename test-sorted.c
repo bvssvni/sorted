2e47ab95984954ebd55fbb6a8adac6b0c3932063
Sven Nilsen <sven@cutoutpro.com>
2012-12-09 23:09:33 +0100

    Added more unit tests.

--- test-sorted.c
+++ test-sorted.c
@@ -42,16 +42,48 @@ void test_expansion_1(void)
 	
 	sorted *list = double_sorted_New(0, double_Compare);
 	assert(list->items.cap == 0);
+	assert(list->sortedindices.cap == 0);
+	assert(list->items.len == 0);
+	assert(list->sortedindices.len == 0);
 	PushValue(list, 1);
 	assert(list->items.cap == 1);
+	assert(list->sortedindices.cap == 1);
+	assert(list->items.len == 1);
+	assert(list->sortedindices.len == 1);
 	PushValue(list, 2);
 	assert(list->items.cap == 2);
+	assert(list->sortedindices.cap == 2);
+	assert(list->items.len == 2);
+	assert(list->sortedindices.len == 2);
 	PushValue(list, 3);
 	assert(list->items.cap == 4);
+	assert(list->sortedindices.cap == 4);
+	assert(list->items.len == 3);
+	assert(list->sortedindices.len == 3);
 	PushValue(list, 4);
 	assert(list->items.cap == 4);
+	assert(list->sortedindices.cap == 4);
+	assert(list->items.len == 4);
+	assert(list->sortedindices.len == 4);
 	PushValue(list, 5);
 	assert(list->items.cap == 8);
+	assert(list->sortedindices.cap == 8);
+	assert(list->items.len == 5);
+	assert(list->sortedindices.len == 5);
+	gcSet(list, NULL);
+}
+
+void test_pop_1(void)
+{
+	typedef double_sorted sorted;
+	func(double_sorted, PushValue);
+	func(double_sorted, Pop);
+	
+	sorted *list = double_sorted_New(10, double_Compare);
+	PushValue(list, 3);
+	assert(Pop(list) == 3);
+	assert(list->items.len == 0);
+	assert(list->sortedindices.len == 0);
 	gcSet(list, NULL);
 }
 
@@ -62,6 +94,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < end; i++) {
 		test_double_sorted_1();
 		test_expansion_1();
+		test_pop_1();
 	}
 	return 0;
 }
